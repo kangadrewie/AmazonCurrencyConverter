78bf7c22d9b2fdf45ee453f308a5dae21002e28c
Andrew Gorman <andrewgorman101@gmail.com>
2020-03-31 16:10:29 +0100

    only one fetch per page load

--- background.js
+++ background.js
@@ -1,22 +1,56 @@
-chrome.tabs.onUpdated.addListener(function (tabId , info) {
+function fetchRates() {
+	console.log('fetched')
+	fetch('https://api.exchangeratesapi.io/latest?base=GBP')
+		.then((response) => {
+			return response.json();
+		})
+	.then((data) => {	
+		return data.rates['EUR'];
+	  })
+	.then((currency) => {
+		chrome.tabs.onUpdated.addListener(function (tabId , info) {
+			if (info.status === 'loading') {
 
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				var activeTab = tabs[0];
+				chrome.tabs.sendMessage(activeTab.id, {"status" : "loading", "currency": currency});
 
-    // Send a message to the active tab
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"message": "page_loading"});
-		});
-
-	if (info.status === 'complete') {
-
-		// Send a message to the active tab
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			var activeTab = tabs[0];
-			chrome.tabs.sendMessage(activeTab.id, {"message": "page_complete"});
 			});
-		}
+
+			}
+
+			if (info.status === 'complete') {
+
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				var activeTab = tabs[0];
+				chrome.tabs.sendMessage(activeTab.id, {"status" : "complete"});
+
+				});	
+			}
+		
+
+		});
+	});
+}
+
+fetchRates();
+
+// chrome.tabs.onUpdated.addListener(function (tabId , info) {
+//     // Send a message to the active tab
+// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 		var activeTab = tabs[0];
+// 		chrome.tabs.sendMessage(activeTab.id, {content: "message", type: "m2"});
+// 		});
+
+// 	if (info.status === 'complete') {
+
+// 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 			var activeTab = tabs[0];
+// 			chrome.tabs.sendMessage(activeTab.id, {content: "message", type: "m3"});
+// 			});
+// 		}
 
 		
-});
+// });
 
 
--- content.js
+++ content.js
@@ -1,53 +1,75 @@
+let currency;
+
 function conversion() {
-	console.log('converted');
 	x = document.querySelectorAll('.a-color-price, .p13n-sc-price, .a-text-strike, .cost-after-savings, .olp-from, .a-color-base, .a-price-whole');
-	currency = 1.18;
 	Array.from(x).forEach((element, index) => {
+		// console.log(currency)
 		element = x[index].innerHTML
 		element_stripped = element.trim()
+
 		if (element_stripped.startsWith('£')) {
-			
+
 			price = x[index].innerHTML;
 			price_cleaned = price.replace(/\s/g&&',', "");
 			price_stripped = price_cleaned.substring(1, price.length);
 
 			conversion = (parseFloat(price_stripped) * currency).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 			x[index].innerHTML = '€' + conversion.toString();
-		}
-	});
+			}
+	});  		
 }
 
-function fetchRates() {
-	console.log('fetched')
-	fetch('https://api.exchangeratesapi.io/latest?base=GBP')
-		.then((response) => {
-			return response.json();
-		})
-	.then((data) => {
-		eur = data.rates['EUR'];
-		// setInterval(hello, 500);
-	  });
-}
-
-
-function hello() {
-	console.log('hello');
-}
-i = 1;
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	
-  	i++;
-  	console.log(i);
-  	if (i == 2) {
-    	intervalId = setInterval(conversion, 5);
-  	}
 
-	if (request.message === 'page_complete') {
-		clearInterval(intervalId);
-  		console.log('completed');
-  		conversion(eur);
-  		i=1;
+	function init(request) {
+
+		if (request.status === 'loading') {
+			currency = request.currency
+			//Fire every 5 ms until page has loaded
+			fireId = setInterval(conversion, 5);
+
+
+
 		}
-	}
-);
+
+		if (request.status === 'complete') {
+			
+			// clearInterval(fireId);
+			console.log('complete')
+
+		}
+
+});
+
+
+// chrome.runtime.onMessage.addListener(
+// 	requestCurrency = (request, sender, sendResponse) => {
+// 		if (request.type != "m2" & request.type != "m3") {
+// 			chrome.extension.onMessage.removeListener(requestCurrency);
+// 			var currency = request.message;
+			
+// 			return conversion(currency)
+// 		}
+// 	}
+// );
+
+
+// i = 1;
+// chrome.extension.onMessage.addListener(
+//   function fireConversion(request, sender, sendResponse, currency) {
+
+//   	if (request.type === "m2") {
+// 	  	i++;
+// 	  	if (i == 2) {
+// 	  		console.log("Start Conversion");
+// 	    	intervalId = setInterval(conversion, 5);
+// 	  	}
+//   	}
+
+// 	if (request.type === "m3") {
+// 		clearInterval(intervalId);
+//   		console.log('Conversion Completed');
+//   		i=0;
+// 		}
+// 	}
+// );
