ffa58b1a00ab0d839ec4942766d5b88b51de540d
Andrew Gorman <andrewgorman101@gmail.com>
2020-04-07 20:06:40 +0100

    pulls rates based on selection

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
-let currencyRate;
 
 
-// popup.js
+
+
 chrome.storage.local.get(["currency", "currencyName"], function(data) {
     if(typeof data.currency == "undefined") {
         // That's kind of bad
@@ -11,3 +11,20 @@ chrome.storage.local.get(["currency", "currencyName"], function(data) {
 
     }
 });
+
+// document.getElementById('options').addEventListener("click", test);
+
+
+//Get Dropdown value
+function getDropdownValue() {
+	var e = document.getElementById("currencies");
+	var strUser = e.options[e.selectedIndex].value;
+	console.log(strUser);
+
+	//Store currency type and value in local storage for background.js to pull when popup is opened.
+	chrome.storage.local.set({currencyName: strUser});
+}
+
+// document.getElementsByClassName('options').addEventListener("click", getDropdownValue);
+document.getElementById('currencies').addEventListener("change", getDropdownValue);
+
